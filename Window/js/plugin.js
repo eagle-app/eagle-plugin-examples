6553c67c48ec8b7de1e243ebd547df5b957256f7
Augus <augus@ogdesign.tw>
2022-12-23 14:17:45 +0800

    Window Plugin Example

---
+++ Window/js/plugin.js
@@ -0,0 +1,25 @@
+eagle.onPluginCreate((plugin) => {
+	console.log('eagle.onPluginCreate');
+	console.log(plugin);
+	document.querySelector('#message').innerHTML = `
+	<ul>
+		<li>id: ${plugin.manifest.id}</li>
+		<li>version: ${plugin.manifest.version}</li>
+		<li>name: ${plugin.manifest.name}</li>
+		<li>logo: ${plugin.manifest.logo}</li>
+		<li>path: ${plugin.path}</li>
+	</ul>
+	`;
+});
+
+eagle.onPluginShow(() => {
+	console.log('eagle.onPluginShow');
+});
+
+eagle.onPluginHide(() => {
+	console.log('eagle.onPluginHide');
+});
+
+eagle.onPluginBeforeExit((event) => {
+	console.log('eagle.onPluginBeforeExit');
+});
